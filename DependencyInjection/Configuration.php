cae906c6942a9e19bad5c1197489ba7722a30dc5
Nikita Pushkar <nikitospush@gmail.com>
2020-03-04 09:38:06 +0100

    Fix di config

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -15,15 +15,8 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritdoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('sgk_barcode');
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
-        return $treeBuilder;
+        return new TreeBuilder('sgk_barcode');
     }
 }
